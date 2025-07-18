a046de5c937c67c125668f5e31de099983e99411
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2025-07-18 12:57:38 +0100

    fixing database connection issue

--- db.js
+++ db.js
@@ -6,7 +6,16 @@ console.log('DB Host:', process.env.MYSQL_HOST || process.env.MYSQLHOST);
 console.log('DB User:', process.env.MYSQL_USER || process.env.MYSQLUSER);
 console.log('DB Name:', process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE);
 
-const sequelize = new Sequelize(
+let sequelize;
+
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: 'mysql' });
+} else {
+  console.log('DATABASE_URL not set — skipping DB connection');
+}
+
+sequelize = new Sequelize(
   process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE,
   process.env.MYSQL_USER || process.env.MYSQLUSER,
   process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD,
